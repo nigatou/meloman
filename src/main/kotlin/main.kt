769be2a83b19ce5934bd36728e535cb6240c1e31
nigatou <wholewhy69@gmail.com>
2020-10-12 00:38:37 +0300

    Fix

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,24 +1,26 @@
 fun main() {
-    offer(100.0, false)
-    offer(34234.0, false)
-    offer(100.0, true)
-    offer(34234.0, true)
+    offer(100.0, 100000, true)
+    offer(3434.0, 5000, false)
+    offer(100.0, 10000, true)
+    offer(34234.0, 15000, false)
 }
 
-fun offer(amount: Double, client: Boolean) {
+fun offer(amount: Double, clientLastPurchase: Int, clientMeloman: Boolean) {
     var finalAMount = amount;
 
-    if (1001 < amount && amount < 10000) {
+    if (clientLastPurchase in 1000..10000) {
         finalAMount -= 100.0;
-    } else if (10001 < amount) {
+    } else if (clientLastPurchase > 10001) {
         finalAMount *= 0.95
     }
 
-    if (client) {
+    if (clientMeloman) {
         finalAMount *= 0.99
     }
 
-    println("Покупка - $amount рублей")
-    println("Покупка со скидкой - $finalAMount рублей")
+
+
+    println("Покупка - ${amount.toString().split(".")[0]} рублей ${amount.toString().split(".")[1]} копеек")
+    println("Покупка со скидкой - ${finalAMount.toString().split(".")[0]} рублей ${finalAMount.toString().split(".")[1]} копеек")
     println()
 }
